d8d6aa5bb94f5aca5fa160e583fa1b90c6da6084
Adrian Rangel <adrian.rangel@gmail.com>
2015-04-30 10:08:38 -0500

    use scrapper.rb as parent class

--- scrapper.rb
+++ scrapper.rb
@@ -2,27 +2,15 @@ require 'mechanize'
 require_relative 'expediente'
 module BuzonLegal
   class Scrapper
-    attr_accessor :expedientes
-    def initialize(date = (Date.today - 1).strftime('%d-%m-%Y'))
-      mechanize = Mechanize.new
+    attr_accessor :page, :expedientes
+    def initialize(args)
+      self.page = Mechanize.new.get(args[:url])
+      self.date = args[:date] || (Date.today - 1).strftime('%d-%m-%Y')
       self.expedientes = []
-      page = mechanize.get('http://cjj.gob.mx/consultas/boletin')
-      form = page.forms.first
-      form['data[Boletin][txtbusqueda]'] = date
-      form['data[Boletin][tipo]'] = 1
-      options = form.fields[2].options
-      options.each do |option|
-        form['data[Boletin][juzgado]'] = option.value
-        data = form.submit
-        table = data.search('table.twelve')
-        uls = table.search('ul.tabs-content')
-        dls = uls.search('dl')
-        dls.each do |dl|
-          dd = dl.search('dd')
-          expedientes << Expediente.new(expediente: dd[0].content.strip, juicio: dd[1].content.strip,
-                                        actores: dd[2].content.strip, acuerdo: dd[3].content.strip)
-        end
-      end
+    end
+
+    def expedientes
+      fail 'Tienes que implementar el metodo expedientes'
     end
   end
 end
